e380df0b61de7c84f08d11150989b3015abb1e32
DomDomiX <domdomix74@gmail.com>
2025-09-30 18:13:27 +0200

    Login GUI tweaks

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TuiButton} from '@taiga-ui/core';
+import {FormsModule} from '@angular/forms';
+import {TuiIcon, TuiTextfield} from '@taiga-ui/core';
+import {TuiPassword} from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [TuiButton, TuiTextfield, FormsModule, TuiIcon, TuiPassword],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  username = '';
+  password = '';
 }
