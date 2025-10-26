8212065195c8d8cc3770239513e4a909e629a7fe
DomDomiX <domdomix74@gmail.com>
2025-10-26 23:25:32 +0100

    Navbar and Login Component Styling Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { TuiRoot } from "@taiga-ui/core";
+import { TuiRoot, TuiIcon } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, TuiIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
