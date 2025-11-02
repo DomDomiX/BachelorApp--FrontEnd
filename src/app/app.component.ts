288fcc4fcf7fc6f5ce9f0cf548727c68502fe7f9
DomDomiX <domdomix74@gmail.com>
2025-11-02 15:13:12 +0100

    Routing in navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { TuiRoot } from "@taiga-ui/core";
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,12 +13,25 @@ export class AppComponent implements OnInit {
   title = 'BachelorApp--FrontEnd';
   loggedIn: boolean = false;
 
+  constructor(private router: Router) {}
+
   ngOnInit() {
+    this.loggedIn = false;
     this.isUserLoggedIn();
   }
 
   isUserLoggedIn(): boolean {
     localStorage.getItem("accessToken") ? this.loggedIn = true : this.loggedIn = false;
     return this.loggedIn;
+  }
+
+  isActiveRoute(route: string): boolean {
+    return this.router.url === route;
+  }
+
+  logout() {
+    localStorage.removeItem('accessToken');
+    this.loggedIn = false;
+    this.router.navigate(['/login']);
   }
 }
